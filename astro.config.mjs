52eb88f451f34136be50bbc554e6b5e09311b0ca
Rob N <rob.netzke@gmail.com>
2024-09-15 08:35:39 -1000

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://robnetzke.com",
+  site: "https://rustaceanrob.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
